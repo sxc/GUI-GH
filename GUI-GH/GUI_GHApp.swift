2bd91eb70127a5edaff0dcc3178c8f59863e7e88
sxc <qiuyequ@icloud.com>
2022-06-21 13:36:11 +0800

    Initial Commit

---
+++ GUI-GH/GUI_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUI_GHApp.swift
+//  GUI-GH
+//
+//  Created by Xiaochun Shen on 2022/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GUI_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
